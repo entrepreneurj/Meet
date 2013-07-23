e27cd229067f7c60c3fce3c8b1070a8a800f1714
Joseph Alexander Lee Letts <Joe@home.westell.com>
2013-07-23 06:39:10 +0100

    Revert "trying to find db host"
    This reverts commit 4e25b3edd98ab279baaeb1752398ff8114c56140.
    
    Realised no external access to DB

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -2,6 +2,4 @@ import os
 from django.shortcuts import render_to_response
 
 def home(request):
-
-    host=os.environ['OPENSHIFT_MYSQL_DB_HOST'];
-    return render_to_response('home/home.html', {'host':host,})
+    return render_to_response('home/home.html')
